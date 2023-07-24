ec4e637223474a90f2b8b8832a56264605ed3d17
Mishika Jaiswal <mishikaj162001@gmail.com>
2023-07-24 11:47:10 +0530

    deployment code added

--- backend/index.js
+++ backend/index.js
@@ -25,31 +25,18 @@ app.use(cors())
 
 app.use('/razorpay', paymentRoute);
 
+// DEPLOYMENT
+app.use(express.static(path.join(__dirname, "./frontend/build")))
 
-// app.post('/razorpay', async(req, res) => {
-//     const payment_capture = 1
-//     const amount = parseInt(req.query.price) * 100
-//     const currency = 'INR'
-
-//     const options = {
-//         amount: amount,
-//         currency: currency,
-//         receipt: shortid.generate(),
-//         payment_capture
-//     }
-
-//     try {
-//         const response = await razorpay.orders.create(options)
-//         console.log(response)
-//         res.json({
-//             id: response.id,
-//             currency: response.currency,
-//             amount: response.amount
-//         })
-//     } catch (error) {
-//         console.log(error)
-//     }
-// })
-
+app.get('*', function(_, res) {
+    res.sendFile(
+        path.join(__dirname, "./frontend/build/index.html"),
+        function(err) {
+            if(err) {
+                res.status(500).send(err);
+            }
+        }
+    )
+})
 app.listen(server_port, 
     console.log(`SERVER STARTED ON PORT ${server_port}`.brightBlue.bold))
