a7fb6f68b90ccb5049453713cf558acc9c984a32
Mishika Jaiswal <mishikaj162001@gmail.com>
2023-07-24 12:06:00 +0530

    code updated

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const cors = require('cors');
+const path = require('path');
 
 const userRoutes = require('./routes/userRoutes');
 const paymentRoute = require('./routes/paymentRoute');
