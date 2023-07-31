c06616d83ca50acb4ef67692b32cc3e0a50bb70b
Mishika Jaiswal <mishikaj162001@gmail.com>
2023-07-31 22:20:54 +0530

    contact page updated

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,14 +1,13 @@
 export const Contact = () => {
     return (
-        <div className="h-[100vh] justify-center items-center p-4">
-            <h1 className="text-4xl">Contact Us</h1>
+        <div className="h-[100vh] justify-center items-center">
             
             <div className="flex justify-content items-center w-full">
                 <div className="w-1/2">
-                    <p>Hello</p>
+                    <h1 className="text-4xl"><span>Contact </span><span>Us</span></h1>
                 </div>
                 <div className="w-1/2">
-                    <img src="/assets/contact.png" alt="" />
+                    <img className="h-[100vh]" src="assets/contact.jpg" alt="image" />
                 </div>
             </div>
         </div>
