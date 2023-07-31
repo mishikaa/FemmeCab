2b446f55aedfdac62a1a9cadabc0c2ea96f81a6b
Mishika Jaiswal <mishikaj162001@gmail.com>
2023-07-31 18:11:19 +0530

    deployment code updated

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const __dirname1 = path.resolve();
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname1, "/frontend/build")))
     app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname1, "frontend", "build", "index.html"))
     });
 } else {
     app.get('/', (req, res) => {
