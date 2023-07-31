69bba2073c3554990686bf16ed9cee93f30cb187
Mishika Jaiswal <mishikaj162001@gmail.com>
2023-07-31 18:43:38 +0530

    db error resolved

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const colors = require('colors');
 
 const connectDB = async() => {
     try {
-        const connection = mongoose.connect(process.env.MONGO_URI, {
+        const connection = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
