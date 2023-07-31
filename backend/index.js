fbb55889b5657c9175b275b77ed23fc220068663
Mishika Jaiswal <mishikaj162001@gmail.com>
2023-07-31 19:37:55 +0530

    favicon added

--- backend/index.js
+++ backend/index.js
@@ -23,10 +23,10 @@ app.use(cors())
 app.use('/razorpay', paymentRoute);
 
 // DEPLOYMENT
-const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === "production") {
-    app.use("/", express.static(path.join(__dirname1, "frontend", "build")))
-    app.get('*', (req, res) => {
+if(process.env.NODE_ENV == "production") {
+    const __dirname1 = path.resolve();
+    app.use(express.static(path.resolve(__dirname1, "frontend", "build")))
+    app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
     });
 } else {
