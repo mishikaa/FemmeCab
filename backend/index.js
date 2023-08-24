f78ba9f9082c91f1a22b1c203e7c2592f16a7bad
Mishika Jaiswal <mishikaj162001@gmail.com>
2023-08-25 00:30:26 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,14 @@ app.use(cors({
     methods: ["POST, GET"]
 }))
 
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
+
 app.use(express.json());
 
 // Different api calls
