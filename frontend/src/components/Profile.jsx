2a7771605743cdcee8dda2e6338caf33f87d6ae8
Mishika Jaiswal <mishikaj162001@gmail.com>
2023-08-25 03:18:43 +0530

    Update Profile.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,20 +1,20 @@
-import { Avatar, Button } from '@chakra-ui/react'
+import { Avatar } from '@chakra-ui/react'
 import React from 'react'
 import { RideState } from '../Context_API/provider'
 import { useNavigate } from 'react-router-dom';
 
-const Profile = () => {
+const Profile = ({doNavigate}) => {
     const navigate = useNavigate() 
     const {user} = RideState();  
     return (
        user && 
        (
         <Avatar 
-          className='p-0.5 m-2 cursor-pointer border-2 shadow-[0_3px_10px_#4f5d75]'
+          className={`p-0.5 m-2 border-2 shadow-[0_3px_10px_#4f5d75] ${doNavigate} && cursor-pointer`}
           name={user.email}
           src={user.profilePhoto !== 'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg' && user.profilePhoto}
           bg='#264653' 
-          onClick={()=>{navigate('/profile')}}
+          onClick={()=>{doNavigate && navigate('/profile')}}
         />
       )
   )
